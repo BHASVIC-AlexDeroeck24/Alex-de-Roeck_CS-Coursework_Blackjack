a7e50e60e9d834eb3ecfc9f71004a36af5e3f791
Alex de Roeck <alex.deroeck24@students.bhasvic.ac.uk>
2025-11-05 10:00:03 +0000

    Added to hands

--- Blackjack Game + Learner - Alex de Roeck/Hand.java
+++ Blackjack Game + Learner - Alex de Roeck/Hand.java
@@ -1,4 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList; // For array lists
+import java.util.Collections;   // Used to randomise the order of data in an ArrayList
 
 /**
  * Write a description of class Hand here.
@@ -12,38 +14,55 @@ public class Hand extends Actor
     int card1Value = 0;
     int card2Value = 0;
     int currentHand = 1;
-    
+    ArrayList<Hand> handsQueue = new ArrayList<Hand>();
+
+    //  Have a queue for the hands.
+    //  This is so I can access the hands from first to last
+    //  Will need to check if the queue is empty.
+
     public void act()
     {
         // Add your action code here.
     }
-    
-    public void createHand()
-    {
-        //Create a hand, ArrayList to store all card objects in the hand
-        ArrayList<Card> cardsInHand = new ArrayList<Card>();
+
+    public Hand (int NEWhandTotal, int NEWcard1Value, int NEWcard2Value){
+        handTotal = NEWhandTotal;
+        card1Value = NEWcard1Value;
+        card2Value = NEWcard2Value;
+    }
+
+    public Hand getCurrentHand(){
+        handsQueue.trimToSize(); // Removes any empty spaces in the queue
+        Hand currentHand = handsQueue.get(0);
+        // handsQueue.remove(firstHand);
+        return currentHand;
     }
     
-    public void hit(currentHand)
+    public void createHand(int card1val, int card2val)
+    {
+        //Create a hand
+        handsQueue.add(new Hand(card1val + card2val, card1val, card2val));
+    }
+
+    public void hit(Hand getCurrentHand)
     {
         //
         dealCard();
     }
-    
-    public void stand(currentHand)
+
+    public void stand(Hand getCurrentHand)
     {
         //
     }
-    
-    public void split(currentHand)
+
+    public void split(Hand getCurrentHand)
     {
         //
     }
-    
-    public void doubleDown(currentHand)
+
+    public void doubleDown(Hand getCurrentHand)
     {
         //
     }
-    
-    
+
 }
